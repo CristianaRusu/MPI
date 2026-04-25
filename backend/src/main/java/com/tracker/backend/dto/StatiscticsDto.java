d8d1f602d652c7431b7485a57add4684e6decb7d
crusu <crusu@stud.hs-heilbronn.de>
2026-04-25 08:37:48 +0300

    SCRUM-31: deleted comment

--- backend/src/main/java/com/tracker/backend/dto/StatiscticsDto.java
+++ backend/src/main/java/com/tracker/backend/dto/StatiscticsDto.java
@@ -4,12 +4,7 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
-/*
-distanța totală alergată
-timpul total de alergare
-pace-ul mediu
-numărul total de alergări
- */
+
 @Data
 public class StatiscticsDto {
     private LocalDateTime totalTime;
