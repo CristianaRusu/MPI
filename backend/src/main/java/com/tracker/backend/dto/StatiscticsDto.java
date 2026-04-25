250702bb88f03479e0ef36584feb76fa0a028d75
crusu <crusu@stud.hs-heilbronn.de>
2026-04-25 09:29:30 +0300

    SCRUM-31: updated

--- backend/src/main/java/com/tracker/backend/dto/StatiscticsDto.java
+++ backend/src/main/java/com/tracker/backend/dto/StatiscticsDto.java
@@ -2,14 +2,6 @@ package com.tracker.backend.dto;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
-/*
-distanța totală alergată
-timpul total de alergare
-pace-ul mediu
-numărul total de alergări
- */
 @Data
 public class StatiscticsDto {
     private Long totalTime;
